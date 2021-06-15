d9bf87b0d9ee116abb2858e3b9ddeb7f97139f94
Elias <Ibarraeliaskc@gmail.com>
2021-06-15 16:39:45 -0300

    actualizacion pracica Node

---
+++ Node/math.js
@@ -0,0 +1,22 @@
+function add(x1, x2){
+
+return x1 + x2;
+}
+
+function substain (x1, x2){
+return x1 - x2;
+}
+
+function multiply (x1, x2) {
+
+return x1 * x2;
+}
+
+function divide (x1, x2){
+
+return x1 / x2;
+}
+
+console.log(substain(1,2));
+
+exports.add =  add; 
